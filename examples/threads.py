49d45139e53866870643a95d20b303ddc815b20d
Iulia Chiriac <iulia.chiriac@yahoo.com>
2025-05-29 16:50:07 +0300

    Add threading &  multiproc examples

---
+++ examples/threads.py
@@ -0,0 +1,28 @@
+import threading
+import time
+
+
+def func(nr):
+    time.sleep(3)
+    print(f"Finished execution from thread {nr}")
+
+
+time_start = time.time()
+threads = []
+
+for i in range(1, 6):
+    t = threading.Thread(target=func, args=(i,))
+    threads.append(t)
+    t.start()  # target(*args, **kwargs)
+
+    print(f'Thread {t.name} alive status: {t.is_alive()}')
+
+    count = threading.active_count()
+    print(f"Total no of threads: {count}")
+
+for thread in threads:
+    thread.join()
+
+time_end = time.time()
+total_time = time_end - time_start
+print(f'Total time: {total_time:.10f}')
